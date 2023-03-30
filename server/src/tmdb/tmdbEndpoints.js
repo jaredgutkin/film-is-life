46e50cb7190be26264d55fdcfd88743865f99fbc
Jared Gutkin <jgutkinsam@gmail.com>
2023-03-29 23:52:39 -0500

    tmdb api set up

--- server/src/tmdb/tmdbEndpoints.js
+++ server/src/tmdb/tmdbEndpoints.js
@@ -1,10 +1,37 @@
-const baseUrl = process.env.TMDB_BASE_URL;
-const key = process.env.TMDB_KEY;
+import tmdbConfig from "./tmdbConfig.js";
 
-const getUrl = (endpoint, params) => {
-    const qs = new URLSearchParams(params);
-
-    return `${baseUrl}${endpoint}?api_key=${key}&${qs}`;
-};
-
-export default { getUrl };
+const tmdbEndpoints = {
+    mediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaCategory}`, { page }
+    ),
+    mediaDetail: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaId}`
+    ),
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
+      `genre/${mediaType}/list`
+    ),
+    mediaCredits: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaId}/credits`
+    ),
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecommend: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaId}/recommendations`
+    ),
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+      `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+      `search/${mediaType}`, { query, page }
+    ),
+    personDetail: ({ personId }) => tmdbConfig.getUrl(
+      `person/${personId}`
+    ),
+    personMedias: ({ personId }) => tmdbConfig.getUrl(
+      `person/${personId}/combined_credits`
+    ),
+  };
+  
+  export default tmdbEndpoints;
+  
