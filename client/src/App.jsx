6f8fd2d5d03036725971e6b08542241a2f3b0b3d
Jared Gutkin <jgutkinsam@gmail.com>
2023-04-07 19:48:42 -0500

    layout set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import MainLayout from './components/layout/MainLayout.jsx';
 
 
 
@@ -7,9 +8,9 @@ function App() {
 
   return (
     <div className="App">
-      hello world
+      <MainLayout />
     </div>
   )
 }
 
-export default App
+export default App;
--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
+import GlobalLoading from "../common/GlobalLoading";
+
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <GlobalLoading />
   )
 }
 
