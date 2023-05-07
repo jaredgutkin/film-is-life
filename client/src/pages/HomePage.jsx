a074390f3d35e9c4f607ecd60b3768694fa70fb5
Jared Gutkin <jgutkinsam@gmail.com>
2023-05-07 13:14:32 -0500

    working on home page slides

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import HeroSlide from "../components/common/HeroSlide.jsx";
+import tmdbConfigs from "../api/config/tmdbConfig.js";
+
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <HeroSlide mediaType={tmdbConfigs.mediaType.movie} mediaCategory={tmdbConfigs.mediaCategory.popular} />
+
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+        {/* <Container header="popular movies">
+          <MediaSlide mediaType={tmdbConfigs.mediaType.movie} mediaCategory={tmdbConfigs.mediaCategory.popular} />
+        </Container>
+
+        <Container header="popular series">
+          <MediaSlide mediaType={tmdbConfigs.mediaType.tv} mediaCategory={tmdbConfigs.mediaCategory.popular} />
+        </Container>
+
+        <Container header="top rated movies">
+          <MediaSlide mediaType={tmdbConfigs.mediaType.movie} mediaCategory={tmdbConfigs.mediaCategory.top_rated} />
+        </Container>
+
+        <Container header="top rated series">
+          <MediaSlide mediaType={tmdbConfigs.mediaType.tv} mediaCategory={tmdbConfigs.mediaCategory.top_rated} />
+        </Container> */}
+      </Box>
+    </>
+  );
 }
 
 export default HomePage
